b0f1dfd52992369c2d6feab7ace1626880802c6e
Guyo <guyo.halake@students.moringaschool.com>
2023-10-12 19:43:11 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the string to lowercase and remove non-letter characters
+  const cleanWord = word.toLowerCase().replace(/[^a-z]/g, '');
+
+  // Compare the cleaned string with its reverse
+  return cleanWord === cleanWord.split('').reverse().join('');
 }
-
-/* 
-  Add your pseudocode here
-*/
-
 /*
   Add written explanation of your solution here
+  - The function takes an input string `word`.
+  - It first converts the string to lowercase to ensure case-insensitive comparison.
+  - It then removes any non-letter characters by using a regular expression.
+  - The cleaned string is compared with its reverse to check if it's a palindrome.
+  - If the original and reversed strings match, it returns true, indicating a palindrome. Otherwise, it returns false.
 */
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +21,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("A man a plan a canal Panama"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("Was it a car or a cat I saw?"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("deified"));
 }
 
 module.exports = isPalindrome;
